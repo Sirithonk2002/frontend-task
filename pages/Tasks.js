a7d235a449e48cca81bd725e762859be7aa64fb9
Your Name <you@example.com>
2025-04-05 00:36:59 +0700

    Update  with new configurations

--- pages/Tasks.js
+++ pages/Tasks.js
@@ -264,7 +264,7 @@ export default function TasksPage() {
   
   return (
     <div
-        className="p-6 min-h-screen bg-cover bg-center"
+        className="p-6 min-h-screen bg-cover bg-center sm:bg-fixed"
         style={{
           background: "linear-gradient(to bottom right, #FF9999, #FFCCCC, #FFFFFF)",	
         }}
